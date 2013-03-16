3c7ba2f4547ce1d76f75dad63b064f6a9b9070d8
Dirk Vanden Boer <dirk.vdb@gmail.com>
2013-03-16 14:46:09 +0100

    Makefiles added that create dummy executable

---
+++ src/main.cpp
@@ -0,0 +1,91 @@
+//    Copyright (C) 2013 Dirk Vanden Boer <dirk.vdb@gmail.com>
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#include <csignal>
+#include <cerrno>
+#include <string>
+#include <cstring>
+
+#include "utils/log.h"
+
+static bool set_signal_handlers();
+
+using namespace utils;
+
+int main(int argc, char **argv)
+{
+#ifndef WIN32
+    if (!set_signal_handlers())
+    {
+        return -1;
+    }
+#endif
+
+    log::info("Doozy");
+
+    return 0;
+}
+
+static void sigterm(int signo)
+{
+    // TODO: cancel running threads
+}
+
+#ifndef WIN32
+static bool set_signal_handlers()
+{
+    struct sigaction sa;
+
+    sa.sa_flags = 0;
+    sa.sa_handler = sigterm;
+    sigemptyset(&sa.sa_mask);
+    sigaddset(&sa.sa_mask, SIGQUIT);
+    sigaddset(&sa.sa_mask, SIGTERM);
+
+    if (sigaction(SIGINT, &sa, nullptr) < 0)
+    {
+        log::error("Can't catch SIGINT: %s", strerror(errno));
+        return false;
+    }
+
+    sa.sa_flags = 0;
+    sa.sa_handler = sigterm;
+    sigemptyset(&sa.sa_mask);
+    sigaddset(&sa.sa_mask, SIGINT);
+    sigaddset(&sa.sa_mask, SIGTERM);
+
+    if (sigaction(SIGQUIT, &sa, nullptr) < 0)
+    {
+        log::error("Can't catch SIGQUIT: %s", strerror(errno));
+        return false;
+    }
+
+    sa.sa_handler = sigterm;
+    sigemptyset(&sa.sa_mask);
+    sigaddset(&sa.sa_mask, SIGINT);
+    sigaddset(&sa.sa_mask, SIGQUIT);
+    sa.sa_flags = 0;
+
+    if (sigaction(SIGTERM, &sa, nullptr) < 0)
+    {
+        log::error("Can't catch SIGTERM: %s", strerror(errno));
+        return false;
+    }
+
+    return true;
+}
+
+#endif
